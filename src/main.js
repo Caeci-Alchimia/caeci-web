bf2d8314f634aadbad1cb9e5616ee06951ef1867
renatoexpert <renatorraraujo@gmail.com>
2022-06-20 23:43:38 -0300

    doing reverse steps

--- src/main.js
+++ src/main.js
@@ -13,17 +13,14 @@ class Element {
 	get_symbol	= ()=> `<spam class='symbol'>${this.symbol}</spam>`;
 	get_name	= ()=> `<spam class='number'>${this.number}</spam>`;
 	get_cell	= ()=> {
-		let	content	= get_number() + get_symbol;
-		`<td class='${this.family}'>${content}</td>`;
+		let	content	= this.get_number() + this.get_symbol();
+		return `<td class="${this.family}">${content}</td>`;
 	}
 }
 
 class EmptyCell extends Element {
 	constructor (x, y) {
-		this.number	= null;
-		this.symbol	= this.name	= this.family	= 'empty';
-		this.x		= x;
-		this.y		= y;
+		super(null, 'empty', 'empty', x, y, 'empty')
 	}
 }
 
@@ -36,22 +33,78 @@ class Table {
 		this.lines.forEach (line => {
 			stack += '<tr>';
 			line.forEach (element => {
-				stack += '<td>';
-				stack += element.symbol;
-				stack += '</td>';
+				stack += element.get_cell();
 			})
 			stack += '</tr>';
 		});
 		stack += '</table>';
+		canva.innerHTML = stack;
 	}
 }
 
 //	Defining
 const	canva		= document.getElementById ('canva')
-canva.innerHTML = 'ola mundo'
+const	address = 'http://localhost:8000/data/elements.csv';
+get_csv (address);
 
+
+//	Prepare lines as tr's
+function make_tr (matrix) {
+	full_stack = '<table>';
+	Object.entries(matrix).forEach (([line_number, element]) => {
+		line_stack = '<tr>'
+		range	= Array.apply (null, Array(18)).map (function (_, i) {return i;});
+		range.forEach ( i => {
+			column = ++i;
+			line_stack += matrix[line_number][column];
+		});
+		line_stack += '</tr>'
+		full_stack += line_stack;
+	});
+	full_stack += '</table>'
+	canva.innerHTML = full_stack;
+}
+//	Putting an empty space where theres no element
+function fullfill (matrix) {
+	let empty	= new EmptyCell ();
+	Object.entries(matrix).forEach (([line_number, element]) => {
+		range	= Array.apply (null, Array(18)).map (function (_, i) {return i;});
+		range.forEach ( i => {
+			column = ++i;
+			matrix[line_number][column] = matrix[line_number][column] ? matrix[line_number][column] : empty.get_cell()
+		});
+	});
+	make_tr (matrix)
+}
+//	Organizing each element on its right line
+function organize_lines (elements) {
+	var organized	= {}
+	elements.forEach (element => {
+		let	linenumber	= element.y;
+		let	column		= element.x;
+		organized[linenumber] = organized[linenumber] ? organized[linenumber] : {};
+		organized[linenumber][column] = element.get_cell();
+	});
+	fullfill (organized)
+}
+//	Formating csv into elements
+function format (raw) {
+	all_elements	= []
+	rlines	= raw.split ('\n');
+	rlines.forEach (rline => {
+		let	prop	= rline.split(';');
+		let	number	= parseInt (prop[1]),
+			symbol	= prop[2],
+			name	= prop[3],
+			x_pos	= parseInt (prop[4]),
+			y_pos	= parseInt (prop[5]),
+			family	= prop[6];
+		let	element	= new Element (number, symbol, name, x_pos, y_pos, family);
+		all_elements.push (element)
+	})
+	organize_lines (all_elements);
+}
 //	Loading datasheet
-
 function get_csv (address) {
 	let	request	= new XMLHttpRequest ();
 	request.open ('GET', address, true);
@@ -63,20 +116,4 @@ function get_csv (address) {
 		}
 	}
 }
-function format (raw) {
-	lines	= raw.split ('\n');
-	lines.forEach (line => {
-		let	prop	= line.split(';');
-		let	number	= prop[1],
-			symbol	= prop[2],
-			name	= prop[3],
-			x_pos	= prop[4],
-			y_pos	= prop[5],
-			family	= prop[6];
-		let	element	= new Element (number, symbol, name, x_pos, y_pos, family);
-	})
-}
-let address = 'http://localhost:8000/data/elements.csv';
-let csv	= get_csv (address);
-console.info (csv)
 
