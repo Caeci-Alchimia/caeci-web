4b1a8e7cfea8c8dc12a02b20e7ec444413567f5e
renatoexpert <renatorraraujo@gmail.com>
2022-07-26 18:49:17 -0300

    test if alert doesnt allow \n

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ class Element {
 		this.y		= y;
 		this.family	= family;
 	}
-	get_alert	= ()=> 'Name \n Symbol \n Mass \n Number \n Family';
+	//get_alert	= ()=> 'Name \n Symbol \n Mass \n Number \n Family';
+	get_alert	= ()=> 'Name Symbol  Mass  Number  Family';
 	get_number	= ()=> `<spam class='number'>${this.number}</spam>`;
 	get_symbol	= ()=> `<spam class='symbol'>${this.symbol}</spam>`;
 	get_name	= ()=> `<spam class='number'>${this.number}</spam>`;
