e4b220632363dd3b0f94c78000ce97f8bee77ad3
RoVergne <106836186+RoVergne@users.noreply.github.com>
2022-08-14 00:06:15 -0300

    Adicionado informações da tabela

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ class Element {
 	get_symbol	= ()=> `<spam class='symbol'>${this.symbol}</spam>`;
 	get_name	= ()=> `<spam class='number'>${this.number}</spam>`;
 	get_cell	= ()=> {
-		return `<td aria-label="${this.name}" class="${this.family}" onclick="clicked ('${this.name}')">${this.get_symbol()}</td>`;
+		return `<td aria-label="${this.name}" class="${this.family}" onclick="clicked ('${this.name} <Br> Símbolo: ${this.symbol} <Br> Número atômico: ${this.number} <Br> Família: ${this.family} <Br> Massa atômica: ${this.mass}')">${this.get_symbol()}</td>`;
 	}
 	
 }
