0a673b0cc290c547f05a635961807526248337a3
RoVergne <106836186+RoVergne@users.noreply.github.com>
2022-08-13 23:58:21 -0300

    adicionado nomes ao clicar

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ class Element {
 	get_symbol	= ()=> `<spam class='symbol'>${this.symbol}</spam>`;
 	get_name	= ()=> `<spam class='number'>${this.number}</spam>`;
 	get_cell	= ()=> {
-		return `<td aria-label="${this.name}" class="${this.family}" onclick="clicked ('skylab')">${this.get_symbol()}</td>`;
+		return `<td aria-label="${this.name}" class="${this.family}" onclick="clicked ('${this.name}')">${this.get_symbol()}</td>`;
 	}
 	
 }
