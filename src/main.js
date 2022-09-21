15e6faccac86df3b0b2344391e8f6461b54f1126
renatoexpert <renatorraraujo@gmail.com>
2022-09-20 23:10:58 -0300

    * added family2 and at_mass for each element; * made controls for loading screen;

--- src/main.js
+++ src/main.js
@@ -1,22 +1,102 @@
 //	This must be a module
+var currentTabIndex = 0;
+
+function nextTabIndex() {
+	currentTabIndex++;
+	if (currentTabIndex > 118) {
+		currentTabIndex = 1;
+	}
+	elem = document.querySelectorAll(`[tabindex="${currentTabIndex}"]`)[0];
+	elem.focus();
+}
+
+function previousTabIndex() {
+	currentTabIndex--;
+	if (currentTabIndex < 1) {
+		currentTabIndex = 118;
+	}
+	elem = document.querySelectorAll(`[tabindex="${currentTabIndex}"]`)[0];
+	elem.focus()
+}
+
+function sleep(milliseconds) {
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+}
+
+function loadPage () {
+	// swiped-right
+	document.addEventListener('swiped-right', function(e) {
+		// console.log('Swiped right!');
+		nextTabIndex();
+	});
+
+	document.addEventListener('swiped-left', function(e) {
+		// alert('Swiped left!');
+		previousTabIndex();
+	});
+
+	document.onkeydown = function(e) {
+		if (e.key === 'Enter') {
+			document.activeElement.onclick();
+		}
+	};
+
+	screen.orientation.lock('portrait');
+	document.getElementById("slogan").focus();
+
+	loading_page = document.getElementById('loading-page');
+	periodic_table = document.getElementById('periodic-table');
+	
+	sleep(4000);
+
+	// hide loading page
+	loading_page.style.display = 'none';
+	periodic_table.style.display = 'block';
+}
+
+function OnFocusOut() {
+    var currentElement = $get(currentElementId); // ID set by OnFOcusIn
+    var curIndex = currentElement.tabIndex; //get current elements tab index
+    if(curIndex == currentTabIndex) { //if we are on the last tabindex, go back to the beginning
+        curIndex = 0;
+    }
+    var tabbables = document.querySelectorAll(".tabable"); //get all tabable elements
+    for(var i=0; i<tabbables.length; i++) { //loop through each element
+        if(tabbables[i].tabIndex == (curIndex+1)) { //check the tabindex to see if it's the element we want
+            tabbables[i].focus(); //if it's the one we want, focus it and exit the loop
+            break;
+        }
+    }
+}
 
 class Element {
-	constructor (number, symbol, name, x, y, family, at_mass) {
+	constructor (number, symbol, name, x, y, family,family2,acronym, at_mass) {
 		this.number	= number;
 		this.symbol	= symbol;
 		this.name	= name;
 		this.x		= x;
 		this.y		= y;
 		this.family	= family;
-		this.mass	= at_mass
+		this.family2	= family2;
+		this.acronym = acronym;
+		this.mass	= at_mass;
 	}
-	
-	get_information = ()=> `clicked ('${this.name},<Br> Símbolo: ${this.symbol}, <Br> Número atômico: ${this.number}, <Br> Família: ${this.family}, <Br> Massa atômica: ${this.mass}')`;
+
+	get_information = ()=> `clicked ('${this.name}, ${this.family},<Br>${this.family2}, <Br> Símbolo: ${this.acronym}, <Br> Número atômico: ${this.number}, <Br> Massa atômica: ${this.mass}')`;
+
 	get_number	= ()=> `<spam class='number'>${this.number}</spam>`;
 	get_symbol	= ()=> `<spam class='symbol'>${this.symbol}</spam>`;
 	get_name	= ()=> `<spam class='number'>${this.number}</spam>`;
 	get_cell	= ()=> {
-		return `<td aria-label="${this.name}" class="${this.family}" onclick="${this.get_information ()}">${this.get_symbol()}</td>`;
+		if (this.number == null) {
+			return `<td aria-label="${this.name}" class="${this.family}" onclick="${this.get_information ()}">${this.get_symbol()}</td>`;
+		} else {
+			return `<td tabindex="${this.number}" aria-label="${this.name}" class="${this.family}" onclick="${this.get_information ()}">${this.get_symbol()}</td>`;
+		}
 	}
 	
 }
@@ -103,8 +183,10 @@ function format (raw) {
 			x_pos	= parseInt (prop[4]),
 			y_pos	= parseInt (prop[5]),
 			family	= prop[6];
-			at_mass	= prop[7];		
-		let	element	= new Element (number, symbol, name, x_pos, y_pos, family, at_mass);
+			family2= prop[7];
+			acronym= prop[8]
+			at_mass	= prop[9];		
+		let	element	= new Element (number, symbol, name, x_pos, y_pos, family, family2, acronym, at_mass);
 		all_elements.push (element)
 	})
 	organize_lines (all_elements);
